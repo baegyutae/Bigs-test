fa219a018dabf2c0345f1c7a12d6ed1f15aaa404
gyutae <qoxowkd0716@gmail.com>
2024-04-30 17:08:25 +0900

    init commit

---
+++ src/main/java/com/example/bigstest/BigsTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.bigstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigsTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BigsTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bigstest/BigsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bigstest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigsTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
