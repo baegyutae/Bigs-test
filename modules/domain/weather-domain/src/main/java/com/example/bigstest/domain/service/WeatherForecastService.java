93b7ef85255f8f1157de17cf8a369f8699cc2865
gyutae <qoxowkd0716@gmail.com>
2024-05-01 12:57:54 +0900

    Refactor: 데이터 전송을 엔티티에서 Dto로 수정

--- modules/domain/weather-domain/src/main/java/com/example/bigstest/domain/service/WeatherForecastService.java
+++ modules/domain/weather-domain/src/main/java/com/example/bigstest/domain/service/WeatherForecastService.java
@@ -1,8 +1,10 @@
 package com.example.bigstest.domain.service;
 
+import com.example.bigstest.domain.dto.WeatherForecastDto;
 import com.example.bigstest.domain.entity.WeatherForecast;
 import com.example.bigstest.domain.repository.WeatherForecastRepository;
 import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,8 +21,22 @@ public class WeatherForecastService {
     }
 
     @Transactional(readOnly = true)
-    public List<WeatherForecast> findAllWeatherForecasts() {
-        return weatherForecastRepository.findAll();
+    public List<WeatherForecastDto> findAllWeatherForecasts() {
+        return weatherForecastRepository.findAll().stream()
+            .map(this::convertToDto)
+            .collect(Collectors.toList());
+    }
+
+    private WeatherForecastDto convertToDto(WeatherForecast forecast) {
+        return new WeatherForecastDto(
+            forecast.getId(),
+            forecast.getBaseDate(),
+            forecast.getBaseTime(),
+            forecast.getCategory(),
+            forecast.getFcstValue(),
+            forecast.getNx(),
+            forecast.getNy()
+        );
     }
 
     @Transactional(readOnly = true)
